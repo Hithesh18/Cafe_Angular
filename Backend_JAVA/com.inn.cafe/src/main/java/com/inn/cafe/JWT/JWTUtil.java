f20f97f73ea6203c2235d7e84bfd857fd841f897
Hithesh C H <hitheshch2000@gmail.com>
2024-10-05 14:03:13 +0200

    Update JWTUtil.java

--- Backend_JAVA/com.inn.cafe/src/main/java/com/inn/cafe/JWT/JWTUtil.java
+++ Backend_JAVA/com.inn.cafe/src/main/java/com/inn/cafe/JWT/JWTUtil.java
@@ -1,5 +1,8 @@
 package com.inn.cafe.JWT;
 
+import java.util.Date;
+
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import com.google.common.base.Function;
@@ -11,6 +14,15 @@ import io.jsonwebtoken.Jwts;
 public class JWTUtil {
 
 	private String secret="passwordsecret";
+	public String extractUsername (String token)
+	{
+		return extractClaims(token, Claims::getSubject);
+	}
+	
+	public Date extractExpiration (String token)
+	{
+		return extractClaims(token, Claims::getExpiration);
+	}
 	
 	public <T> T extractClaims(String token, Function<Claims, T> claimsResolver)
 	{
@@ -22,4 +34,15 @@ public class JWTUtil {
 	{
 		return Jwts.parser().setSigningKey(secret).parseClaimsJwt(token).getBody();
 	}
+	private Boolean isTokenExpired(String token)
+	{
+		return extractExpiration(token).before(new Date());
+	}
+	
+	public Boolean ValidateToken(String token, UserDetails userDetails)
+	{
+		final String username=extractUsername(token);
+		return(username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+	
 }
