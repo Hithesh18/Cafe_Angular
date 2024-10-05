c921342eed00b79ab5f8170c386ed0842b328a02
Hithesh C H <hitheshch2000@gmail.com>
2024-10-05 13:49:10 +0200

    Update JWTUtil.java

--- Backend_JAVA/com.inn.cafe/src/main/java/com/inn/cafe/JWT/JWTUtil.java
+++ Backend_JAVA/com.inn.cafe/src/main/java/com/inn/cafe/JWT/JWTUtil.java
@@ -2,7 +2,24 @@ package com.inn.cafe.JWT;
 
 import org.springframework.stereotype.Service;
 
+import com.google.common.base.Function;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
 @Service
 public class JWTUtil {
 
+	private String secret="passwordsecret";
+	
+	public <T> T extractClaims(String token, Function<Claims, T> claimsResolver)
+	{
+		final Claims claims=extractAllCLaims(token);
+		return claimsResolver.apply(claims);
+	}
+	
+	public Claims extractAllCLaims(String token)
+	{
+		return Jwts.parser().setSigningKey(secret).parseClaimsJwt(token).getBody();
+	}
 }
